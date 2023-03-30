a4fead7ef304b33bb8935a176f49bdcebf3c025d
abhijeetmohol <abhijeetmohol3101@gmail.com>
2023-03-30 13:13:54 +0530

    first commit

---
+++ assets/main.js
@@ -0,0 +1,15 @@
+// here tryong to use const
+
+ /*const a="50";
+ a="20";
+ console.log(a*a)
+ */
+// //here code can't be executed because a is constant.
+
+const x=60;
+const y=40;
+console.log(x)
+console.log(y)
+console.log("addition of two constant variables is ",x+y);
+console.log("multiplication of two constant variables is ",x*y)
+console.log("division of two constant variables is ",x/y)
